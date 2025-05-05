603344273ee305237acd68e6e0cd67de211a6591
Gaurav Singh <gaurav.singh@appvintech.com>
2025-05-05 15:44:26 +0530

    backend basic setup done

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+const http = require("http");
+const environmentVariables = require("./constants/environmentVariables");
+
+const server = http.createServer(app);
+
+// start the server and listen for incomming requests.
+server.listen(environmentVariables.PORT, () => {
+    console.log(
+        `Server started at http://localhost:${environmentVariables.PORT}`
+    );
+});
