771423d01f84cbd7d2e5fbc996ea501645538e74
Gaurav Singh <gs062254@gmail.com>
2025-06-21 14:19:01 +0530

    addedd some message to display on loading screen - the Loader component that is shown when user open up the application

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LoaderCircle } from "lucide-react";
 
 function Loader() {
+	const messages = [
+		"Verifying your credentials...",
+		"Setting things up for you...",
+		"Syncing your experience...",
+		"Almost ready to go...",
+		"Just a sec, loading securely...",
+	];
+
+	const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentMessageIndex((prevMessageIndex) => (prevMessageIndex + 1) % messages.length);
+		}, 3000); // change message every 2 seconds
+
+		return () => clearInterval(interval); // cleanup on unmount
+	}, []);
+
 	return (
-		<div className="grid place-content-center h-screen backdrop-blur-lg">
-			<LoaderCircle className="size-10 animate-spin" />
+		<div className="grid place-content-center h-screen bg-base-100 backdrop-blur-lg">
+			<div className="flex flex-col items-center justify-center gap-4">
+				<LoaderCircle className="size-10 animate-spin" />
+			<p className="text-center">
+				{messages[currentMessageIndex]}
+			</p>
+			</div>
 		</div>
 	);
 }
